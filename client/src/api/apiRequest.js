016be9c54f40bbbeba66ff044487fd3fd841561c
Ori <odmark2023@gmail.com>
2026-03-23 17:21:49 +0200

    adding the zustand store and create a helper api request function

---
+++ client/src/api/apiRequest.js
@@ -0,0 +1,54 @@
+const BASE_URL = "http://localhost:5000";
+
+async function request(path) {
+  const url = `${BASE_URL}${path}`;
+
+  const res = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+    ...options,
+  });
+
+  if (!res.ok) {
+    throw new Error(`Error in the api request`);
+  }
+  return res.json();
+}
+
+
+export async function fetchAllShelters() {
+  return request("/shelters");
+}
+
+
+// export async function fetchShelterById(id) {
+//   return request(`/shelters/${id}`);
+// }
+
+// export async function createShelter(data) {
+//   return request("/shelters", {
+//     method: "POST",
+//     body: JSON.stringify(data),
+//   });
+// }
+
+// export async function updateShelter(id, data) {
+//   return request(`/shelters/${id}`, {
+//     method: "PUT",
+//     body: JSON.stringify(data),
+//   });
+// }
+
+// export async function patchShelter(id, patch) {
+//   return request(`/shelters/${id}`, {
+//     method: "PATCH",
+//     body: JSON.stringify(patch),
+//   });
+// }
+
+
+// export async function deleteShelter(id) {
+//   return request(`/shelters/${id}`, { method: "DELETE" });
+// }
