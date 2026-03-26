8df51683bc634c9705c956bd53898379d0cbba70
yosef ohayon <yyo6420@gmail.com>
2026-03-26 12:22:28 +0200

    navbar and button

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -8,6 +8,7 @@ import RecenterAutomatically from "../components/map/RecenterAutomatically";
 import RecenterButton from "../components/map/RecenterButton";
 import ShelterMarker from "../components/map/ShelterMarker";
 import RoutingMachine from "../components/map/RoutingMachine";
+import { NavLink } from "react-router";
 
 function MapPage() {
   const [isAutoCenter, setIsAutoCenter] = useState(true);
@@ -23,60 +24,65 @@ function MapPage() {
           <p className="loadingText">מאתר את המיקום שלך...</p>
         </div>
       ) : (
-        <div
-          className="mapDiv"
-          style={{ height: "100%", width: "100%", position: "relative" }}
-        >
-          <MapContainer
-            center={myPosition}
-            zoom={15}
-            scrollWheelZoom={true}
-            className="leaflet-container"
+        <>
+          <div className="mapNavbar">
+            <NavLink to={"/"} className="navbarButton">חזרה לדף הבית 🏡</NavLink>
+          </div>
+          <div
+            className="mapDiv"
+            style={{ position: "relative" }}
           >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+            <MapContainer
+              center={myPosition}
+              zoom={15}
+              scrollWheelZoom={true}
+              className="leaflet-container"
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
 
-            <RecenterAutomatically
-              position={myPosition}
-              isAutoCenter={isAutoCenter}
-              setIsAutoCenter={setIsAutoCenter}
-            />
+              <RecenterAutomatically
+                position={myPosition}
+                isAutoCenter={isAutoCenter}
+                setIsAutoCenter={setIsAutoCenter}
+              />
+
+              {selectedShelter && (
+                <RoutingMachine from={myPosition} to={selectedShelter} />
+              )}
+
+              <Marker position={myPosition} icon={userIcon}>
+                <Popup>אתה נמצא כאן!</Popup>
+              </Marker>
+
+              {shelters
+                ?.filter((s) => s.lat && s.lon)
+                .map((shelter) => (
+                  <ShelterMarker
+                    key={shelter._id || `${shelter.lat}-${shelter.lon}`}
+                    shelter={shelter}
+                    onNavigate={setSelectedShelter}
+                  />
+                ))}
+            </MapContainer>
 
             {selectedShelter && (
-              <RoutingMachine from={myPosition} to={selectedShelter} />
+              <button
+                className="clearRouteButton"
+                onClick={() => setSelectedShelter(null)}
+              >
+                בטל ניווט ✕
+              </button>
             )}
 
-            <Marker position={myPosition} icon={userIcon}>
-              <Popup>אתה נמצא כאן!</Popup>
-            </Marker>
-
-            {shelters
-              ?.filter((s) => s.lat && s.lon)
-              .map((shelter) => (
-                <ShelterMarker
-                  key={shelter._id || `${shelter.lat}-${shelter.lon}`}
-                  shelter={shelter}
-                  onNavigate={setSelectedShelter}
-                />
-              ))}
-          </MapContainer>
-
-          {selectedShelter && (
-            <button
-              className="clearRouteButton"
-              onClick={() => setSelectedShelter(null)}
-            >
-              בטל ניווט ✕
-            </button>
-          )}
-
-          <RecenterButton
-            isAutoCenter={isAutoCenter}
-            onClick={() => setIsAutoCenter(true)}
-          />
-        </div>
+            <RecenterButton
+              isAutoCenter={isAutoCenter}
+              onClick={() => setIsAutoCenter(true)}
+            />
+          </div>
+        </>
       )}
     </div>
   );
